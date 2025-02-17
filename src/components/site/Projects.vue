b413202a10371ef4032a8305f7d2b82261201dc5
Leonardo Silva <donar_leo@hotmail.com>
2025-02-17 11:28:01 -0300

    Corrigindo responsividade projetos

--- src/components/site/Projects.vue
+++ src/components/site/Projects.vue
@@ -27,19 +27,23 @@ const projects = [
       </div>
     </q-intersection>
 
-  <div class="row flex flex-center">
-    <div class="col-1"></div>
-    <div class="col-10 row">
-      <div
-        v-for="(project, index) in projects"
-        :key="index"
-        class="col-lg-4 col-md-6 col-sm-12 flex flex-center"
-      >
-        <q-intersection transition="slide-up" transition-duration="1500" once>
-          <CardProjects :img-project="project.imgProject" :view-project="project.viewProject" />
-        </q-intersection>
+    <div class="row flex flex-center">
+      <div class="col-1"></div>
+      <div class="col-10 row">
+        <div
+          v-for="(project, index) in projects"
+          :key="index"
+          class="col-lg-4 col-md-6 col-sm-12 flex flex-center"
+        >
+          <q-intersection transition="slide-up" transition-duration="1500" once>
+            <CardProjects
+              :img-project="project.imgProject"
+              :view-project="project.viewProject"
+            />
+          </q-intersection>
+        </div>
       </div>
+      <div class="col-1"></div>
     </div>
-    <div class="col-1"></div>
   </div>
 </template>
