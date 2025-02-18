c4b25bf6084dd1889e92041884b936ace43761dc
Kaio <kaiocampus@outlook.com>
2025-02-18 11:23:09 -0300

    Finalizando cabeçalho.

--- src/layouts/site/MainLayout.vue
+++ src/layouts/site/MainLayout.vue
@@ -1,25 +1,15 @@
-<script setup>
-import FooterComponent from "src/components/site/FooterComponent.vue";
-
-const links = [
-  { text: "Inicio", to: "#home" },
-  { text: "Serviços", to: "#services" },
-  { text: "Projetos", to: "#projects" },
-  { text: "Contato", to: "#contact" },
-  { text: "Login", to: "/Login-1" },
-];
-</script>
-
 <template>
   <q-layout view="hHr LpR lFf">
     <q-header reveal class="bg-white shadow-3">
-      <div class="row no-wrap items-center q-pa-lg">
+      <div class="row no-wrap items-center q-pa-lg" v-if="!isMobile">
         <div class="col-4">
           <router-link to="/" class="no-decoration">
             <span class="text-primary text-h5">Logo</span>
           </router-link>
         </div>
-        <nav class="col-8 row justify-evenly items-center q-gutter-x-xl">
+        <nav
+          class="col-8 row justify-evenly items-center q-gutter-x-xl"
+        >
           <router-link
             v-for="link in links"
             :key="link.text"
@@ -30,14 +20,54 @@ const links = [
           </router-link>
         </nav>
       </div>
+      <div v-else class="row no-wrap justify-between q-pa-lg">
+        <div class="col-4">
+          <router-link to="/" class="no-decoration">
+            <span class="text-primary text-h5">Logo</span>
+          </router-link>
+        </div>
+        <nav>
+          <q-btn color="primary" icon="menu">
+        <q-menu>
+          <q-list style="min-width: 100px" v-for="link in links " :key="link.text">
+            <q-item :to="link.to" clickable v-close-popup>
+              {{ link.text }}
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+        </nav>
+      </div>
     </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
-
     <FooterComponent />
   </q-layout>
 </template>
+<script>
+import FooterComponent from "src/components/site/FooterComponent.vue";
+export default {
+  components: {
+    FooterComponent,
+  },
+  data() {
+    return {
+      links: [
+        { text: "Inicio", to: "#home" },
+        { text: "Serviços", to: "#services" },
+        { text: "Projetos", to: "#projects" },
+        { text: "Contato", to: "#contact" },
+        { text: "Login", to: "/Login-1" },
+      ],
+    };
+  },
+    computed:{
+      isMobile(){
+        return this.$q.screen.width < 1050
+      }
+    }
+};
+</script>
 
 <style src="src/css/site/global.css"></style>
