5c59e5d70760009072ee5a679f99b8d383263100
Leonardo Silva <donar_leo@hotmail.com>
2025-02-18 10:30:53 -0300

    Corrigindo scroll smooth com transition

--- src/components/site/About.vue
+++ src/components/site/About.vue
@@ -1,42 +1,51 @@
 <template>
-  <q-card flat class="q-my-lg bg-grey-2">
-    <q-card-section class="no-padding">
-      <div class="row">
-        <div class="col-12 col-md-6">
-          <img
-            src="../../assets/site/img/about.jpg"
-            alt="About Us Image"
-            class="full-width"
-            style="max-height: 500px; object-fit: cover"
-          />
-        </div>
-        <div class="col-12 col-md-6 q-pl-lg">
-          <q-card-section>
-            <h6 class="text-positive no-margin q-pt-md">About Us</h6>
-            <h3 class="no-margin q-py-md">
-              25+ Years Experience In Solar & Renewable Energy Industry
-            </h3>
-            <p class="text-grey-7 q-pt-md">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            </p>
-            <ul class="text-grey-7 q-pb-md">
-              <li>High Quality</li>
-              <li>Customer Satisfaction</li>
-              <li>Innovative Solutions</li>
-            </ul>
-            <q-btn
-              label="Explore More"
-              color="positive"
-              no-caps
-              unelevated
-              rounded
-              size="22px"
-              class="text-h4"
+  <q-intersection
+    transition="fade"
+    transition-duration="1000"
+    once
+    class="intersection-500"
+  >
+    <q-card flat class="q-my-lg bg-grey-2">
+      <q-card-section class="no-padding">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img
+              src="../../assets/site/img/about.jpg"
+              alt="About Us Image"
+              class="full-width"
+              style="max-height: 500px; object-fit: cover"
             />
-          </q-card-section>
+          </div>
+          <div class="col-12 col-md-6 q-pl-lg">
+            <q-card-section>
+              <h6 class="text-positive no-margin q-pt-md">About Us</h6>
+              <h3 class="no-margin q-py-md">
+                25+ Years Experience In Solar & Renewable Energy Industry
+              </h3>
+              <p class="text-grey-7 q-pt-md">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam.
+              </p>
+              <ul class="text-grey-7 q-pb-md">
+                <li>High Quality</li>
+                <li>Customer Satisfaction</li>
+                <li>Innovative Solutions</li>
+              </ul>
+              <q-btn
+                label="Explore More"
+                color="positive"
+                no-caps
+                unelevated
+                rounded
+                size="22px"
+                class="text-h4"
+              />
+            </q-card-section>
+          </div>
         </div>
-      </div>
-    </q-card-section>
-  </q-card>
+      </q-card-section>
+    </q-card>
+  </q-intersection>
 </template>
+
+<style scoped src="../../css/site/global.css"></style>
--- src/components/site/FormComponent.vue
+++ src/components/site/FormComponent.vue
@@ -1,104 +1,112 @@
 <template>
-  <q-card flat class="q-my-lg bg-grey-2">
-    <q-card-section class="no-padding">
-      <div class="row">
-        <div class="col-12 col-md-6">
-          <img
-            src="../../assets/site/img/quote.jpg"
-            alt="About Us Image"
-            class="full-width"
-            style="max-height: 600px; object-fit: cover"
-          />
-        </div>
-        <div class="col-12 col-md-6 q-pl-lg">
-          <q-card-section>
-            <h6 class="text-positive no-margin q-pt-md">Free Quote</h6>
-            <h3 class="no-margin q-py-md">
-              <strong> Get A Free Quote </strong>
-            </h3>
-            <p class="text-grey-7 text-subtitle1 q-pt-md">
-              Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
-              diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
-              lorem sit clita duo justo erat amet
-            </p>
+  <q-intersection
+    transition="fade"
+    transition-duration="1000"
+    once
+    class="intersection-630"
+  >
+    <q-card flat class="q-my-lg bg-grey-2">
+      <q-card-section class="no-padding">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img
+              src="../../assets/site/img/quote.jpg"
+              alt="About Us Image"
+              class="full-width"
+              style="max-height: 600px; object-fit: cover"
+            />
+          </div>
+          <div class="col-12 col-md-6 q-pl-lg">
+            <q-card-section>
+              <h6 class="text-positive no-margin q-pt-md">Free Quote</h6>
+              <h3 class="no-margin q-py-md">
+                <strong> Get A Free Quote </strong>
+              </h3>
+              <p class="text-grey-7 text-subtitle1 q-pt-md">
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
+                Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
+                sed stet lorem sit clita duo justo erat amet
+              </p>
 
-            <div class="q-pa-md">
-              <q-form @submit="onSubmit" class="q-gutter-md">
-                <div class="row q-col-gutter-md">
-                  <div class="col-12 col-md-6">
-                    <q-input
-                      filled
-                      v-model="name"
-                      label="Your name *"
-                      hint="Name and Lastname"
-                      lazy-rules
-                      :rules="[
-                        (val) =>
-                          (val && val.length > 0) || 'Please type something',
-                      ]"
-                    />
+              <div class="q-pa-md">
+                <q-form @submit="onSubmit" class="q-gutter-md">
+                  <div class="row q-col-gutter-md">
+                    <div class="col-12 col-md-6">
+                      <q-input
+                        filled
+                        v-model="name"
+                        label="Your name *"
+                        hint="Name and Lastname"
+                        lazy-rules
+                        :rules="[
+                          (val) =>
+                            (val && val.length > 0) || 'Please type something',
+                        ]"
+                      />
+                    </div>
+                    <div class="col-12 col-md-6">
+                      <q-input
+                        filled
+                        type="email"
+                        label="Your email *"
+                        v-model="email"
+                        lazy-rules
+                        :rules="[
+                          (val) =>
+                            (val && val.length > 0) || 'Please type your email',
+                          (val) =>
+                            /.+@.+\..+/.test(val) ||
+                            'Please type a valid email',
+                        ]"
+                      />
+                    </div>
+                    <div class="col-12 col-md-6">
+                      <q-input
+                        filled
+                        type="tel"
+                        v-model="mobile"
+                        label="Your mobile"
+                      />
+                    </div>
+                    <div class="col-12 col-md-6">
+                      <q-select
+                        filled
+                        v-model="service"
+                        label="Select a service"
+                        :options="[
+                          { label: 'Service 1', value: 'service1' },
+                          { label: 'Service 2', value: 'service2' },
+                          { label: 'Service 3', value: 'service3' },
+                        ]"
+                      />
+                    </div>
                   </div>
-                  <div class="col-12 col-md-6">
-                    <q-input
-                      filled
-                      type="email"
-                      label="Your email *"
-                      v-model="email"
-                      lazy-rules
-                      :rules="[
-                        (val) =>
-                          (val && val.length > 0) || 'Please type your email',
-                        (val) =>
-                          /.+@.+\..+/.test(val) || 'Please type a valid email',
-                      ]"
-                    />
-                  </div>
-                  <div class="col-12 col-md-6">
-                    <q-input
-                      filled
-                      type="tel"
-                      v-model="mobile"
-                      label="Your mobile"
-                    />
-                  </div>
-                  <div class="col-12 col-md-6">
-                    <q-select
-                      filled
-                      v-model="service"
-                      label="Select a service"
-                      :options="[
-                        { label: 'Service 1', value: 'service1' },
-                        { label: 'Service 2', value: 'service2' },
-                        { label: 'Service 3', value: 'service3' },
-                      ]"
-                    />
-                  </div>
-                </div>
 
-                <q-toggle
-                  v-model="accept"
-                  label="I accept the license and terms"
-                />
-
-                <div>
-                  <q-btn
-                    label="Submit"
-                    type="submit"
-                    color="positive"
-                    rounded
-                    unelevated
-                    no-caps
-                    size="22px"
-                    class="q-px-lg text-h4"
+                  <q-toggle
+                    v-model="accept"
+                    label="I accept the license and terms"
                   />
-                </div>
-              </q-form>
-            </div>
-          </q-card-section>
+
+                  <div>
+                    <q-btn
+                      label="Submit"
+                      type="submit"
+                      color="positive"
+                      rounded
+                      unelevated
+                      no-caps
+                      size="22px"
+                      class="q-px-lg text-h4"
+                    />
+                  </div>
+                </q-form>
+              </div>
+            </q-card-section>
+          </div>
         </div>
-      </div>
-    </q-card-section>
-  </q-card>
+      </q-card-section>
+    </q-card>
+  </q-intersection>
 </template>
 
 <script>
@@ -143,3 +151,5 @@ export default {
   },
 };
 </script>
+
+<style scoped src="../../css/site/global.css"></style>
